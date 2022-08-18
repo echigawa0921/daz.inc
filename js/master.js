68ea241d20c1478e183866b8ba8600c83d3abe3a
echigawa0921 <69971834+echigawa0921@users.noreply.github.com>
2022-08-18 20:34:53 +0900

    href

--- js/master.js
+++ js/master.js
@@ -4,3 +4,11 @@ function menuToggle() {
   const header_menu_detail = document.querySelector('.header_nav');
   header_menu_detail.classList.toggle('menu_active');
 }
+
+
+$('#page-link a[href*="#"]').click(function () {
+  var elmHash = $(this).attr('href'); //ページ内リンクのHTMLタグhrefから、リンクされているエリアidの値を取得
+  var pos = $(elmHash).offset().top;  //idの上部の距離を取得
+  $('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
+  return false;
+});
